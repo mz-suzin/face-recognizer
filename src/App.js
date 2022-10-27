cc5166d4cc6ded174dd10f2fec4cf7d4b0d57a4d
mz-suzin <mzsuzin@gmail.com>
2022-10-26 21:18:07 -0300

    minor modifications

--- src/App.js
+++ src/App.js
@@ -168,36 +168,12 @@ class App extends Component {
         .then(count => {
           this.setState(Object.assign(this.state.user, { entries: count}))
         })
-        .catch(console.log('erro aqui viu'))
+        .catch(console.log('Could not find correct definitions'))
       }
       this.displayFaceBox(this.calculateFaceLocation(res))
     })
     .catch(err => console.log(err))
   }
-
-
-
-  //   app.models
-  //     .predict(
-  //       Clarifai.FACE_DETECT_MODEL,
-  //       this.state.input)
-  //     .then(response => {
-  //       if (response) {
-  //         fetch('http://localhost:3000/image', {
-  //           method: 'put',
-  //           headers: {'content-type': 'application/json'},
-  //           body: JSON.stringify({
-  //             id: this.state.user.id
-  //           })
-  //         })
-  //         .then(response => response.json())
-  //         .then(count => {
-  //           this.setState(Object.assign(this.state.user, {entries: count}))
-  //         })
-  //       }
-  //       this.displayFaceBox(this.calculateFaceLocation(response))})
-  //     .catch(err => console.log(err));
-  // }
 
   loadUser = (data) => {
     this.setState({user: {
